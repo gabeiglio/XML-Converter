1f0d6330483ec4ba659415404da0d7cd04a06145
Gabriel Igliozzi <gaboiglio@gmail.com>
2022-10-04 21:43:47 -0400

    Added .gitignore, moved main.py so src directory

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 import xml.etree.ElementTree as et
 
-# xml -> [restrictions per field]* -> html form -> xml
-
-margin = 10
-
+# TODO: Create object wrapper for better manipulation and customization of css properties for now
 def printScope(scope):
     result = ""
     for i in range(0, scope):
@@ -15,7 +12,7 @@ def write(file, input):
 
 def traverse(root, scope, file):
     if (len(root) != 0):
-        write(file, printScope(scope) + "<div style=\"margin: {}px; padding: 20px; border-style: solid; border-color: gray; background-color: #E5E8E8; border-radius: 25px;\">\n".format(margin * scope))
+        write(file, printScope(scope) + "<div style=\"margin: {}px; padding: 20px; border-style: solid; border-color: gray; background-color: #E5E8E8; border-radius: 25px;\">\n".format(10 * scope))
         write(file, printScope(scope) + "<h3>{}</h3>\n".format(root.tag))
     else:
         write(file, printScope(scope) + "<div style=\"margin: 20px;\">\n")
@@ -28,10 +25,11 @@ def traverse(root, scope, file):
         write(file, printScope(scope) + "</div>\n")
 
 def compileHTML(xml_file):
-    #later change to function argument
+    # TODO: later change to function argument
     tree = et.parse("/Users/gabeiglio/Developer/XML-Converter/tests/test1.xml")
     root = tree.getroot()
 
+    # TODO: grab name of input file and create file output with same name
     output = open("output.html", "a")
     traverse(root, 1, output)
     output.close()
